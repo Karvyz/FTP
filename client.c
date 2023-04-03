6119de82b66997b384697b1a45168648b62e8605
Henri Gros <henrigros1@gmail.com>
2023-04-03 20:50:29 +0200

    fix nombre arguments

--- client.c
+++ client.c
@@ -168,7 +168,7 @@ int main(int argc, char **argv) {
     int clientfd;
     char *host;
 
-    if (argc != 3) {
+    if (argc != 2) {
         fprintf(stderr, "usage: %s <host>\n", argv[0]);
         exit(0);
     }
